832a3fa764989b5bcf1a56fa8bee88f1cc3349b9
Justin Guathier <justin@justin-tech.com>
2021-03-16 20:47:41 -0400

    Update to version v0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='vault',
-   version='0.2.2',
+   version='0.2.3',
    description='Helm plugin for storing secrets in HashiCorp Vault',
    author='Just-Insane',
    author_email='justin@justin-tech.com',
